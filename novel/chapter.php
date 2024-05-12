1142a2991d74cd4ef93b857ba90f54720a9afeb3
Aeranga <122707772+Aeranga@users.noreply.github.com>
2024-05-12 12:40:55 +0530

    update

--- novel/chapter.php
+++ novel/chapter.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "your_database_name";
+$dbname = "database_name";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
